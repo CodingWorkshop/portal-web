438ee163fabe3b30c8c72f1a9647c7fbfa68cc45
robben5566 <ccav31104@gmail.com>
2018-11-14 17:20:52 +0800

    引入Font Awesome 5

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p><i class="fas fa-shuttle-van"></i><i class="fas fa-shuttle-van"></i><i class="fas fa-shuttle-van"></i> 微婆 可帶 不養 <i class="fas fa-shuttle-van"></i><i class="fas fa-shuttle-van"></i><i class="fas fa-shuttle-van"></i></p>    
   </div>
 </template>
