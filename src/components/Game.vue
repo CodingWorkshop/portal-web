44bff8b12d23b4b67198bce10461402c615b043b
robben5566 <ccav31104@gmail.com>
2018-11-21 18:06:16 +0800

    練習做遊戲的模板

---
+++ src/components/Game.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="game">
+    <img src="../assets/logo.png">
+    {{name}}
+    <i class="fa-heart" :class="[isLike? 'fas':'far']"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  props: { name: String, isLike: Boolean }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.game {
+  display: inline-block;
+  width: 120px;
+  height: 140px;
+  margin: 5px;
+  padding: 10px;
+  background: rgba($color: #777, $alpha: 0.8);
+}
+.game img {
+  display: block;
+  width: 100%;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -13,5 +13,26 @@
       <i class="far fa-lemon"></i>
       <i class="fas fa-coffee"></i>來一杯蜂蜜檸檬
     </p>
+    <Game v-for="game in games" :key="game.id" :name="game.name" :isLike="game.isLike"/>
   </div>
 </template>
+<script>
+import Game from '@/components/Game.vue';
+export default {
+  name: 'about',
+  data: function() {
+    return {
+      games: [
+        { id: 0, name: '吃飯', imgUrl: '', isLike: true },
+        { id: 1, name: '睡覺', imgUrl: '', isLike: false },
+        { id: 2, name: '打東東', imgUrl: '', isLike: false },
+        { id: 3, name: '????', imgUrl: '', isLike: true },
+        { id: 4, name: 'ㄎㄎㄎ', imgUrl: '', isLike: true }
+      ]
+    };
+  },
+  components: {
+    Game
+  }
+};
+</script>
