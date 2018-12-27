dfc5d8446b0786f12205a6ff9264c34995c54abe
cyan92128505 <cyan92128505@gmail.com>
2018-12-27 14:36:53 +0800

    Modify /script/tasks/pack.js

--- scripts/tasks/pack.js
+++ scripts/tasks/pack.js
@@ -1,12 +1,38 @@
 const {
     spawn
 } = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
+const dayjs = require('dayjs');
 
-if (process.env.NODE_ENV === 'production') {
-    return spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['run', 'build'], {
-        stdio: 'inherit'
-    });
-} else {
-    console.log('Environment is not production.');
-    console.log('Do not execute npm run build.')
+generateCustomStyle();
+prepareENV();
+
+function prepareENV() {
+    if (process.env.NODE_ENV === 'production') {
+        return spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['run',
+            'build'
+        ], {
+            stdio: 'inherit'
+        });
+    } else {
+        console.log(`[${getNow()}][INFO]:Environment is not production.`);
+        console.log(`[${getNow()}][INFO]:Do not execute npm run build.`)
+    }
+}
+
+function generateCustomStyle() {
+    const customStylePath = path.join(
+        process.cwd(),
+        'src',
+        'styles',
+        'variables.scss'
+    );
+    fs.ensureFile(customStylePath).then(() => {
+        console.log(`[${getNow()}][INFO]:generate custom style`);
+    })
+}
+
+function getNow() {
+    return dayjs().format('YYYY-MM-DD HH:mm:ss')
 }
