a17fcda3e93361368366ced7ac9358a3ed8f998b
vito <devil199301@gmail.com>
2019-01-05 17:11:15 +0800

    move directory

---
+++ src/services/router.ts
@@ -0,0 +1,52 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import { Store } from 'vuex';
+
+import Home from '@/views/Home.vue';
+import lobby from '@/views/Lobby.vue';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/lobby',
+      name: 'lobby',
+      component: lobby
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ '@/views/articles/About.vue')
+    }
+  ]
+});
+
+export default (store: Store<any>) => {
+  router.beforeEach(function(to, from, next) {
+    const requiredLogin = to.meta.requiredLogin || false;
+    const loginStatus = requiredLogin && !store.state.login.loginStatus;
+    if (!loginStatus) {
+      next();
+      return;
+    }
+
+    store.commit('openDrawerPage', {
+      type: 'login'
+    });
+    store.commit('recordDestinationPage', to.path);
+  });
+
+  return router;
+};
